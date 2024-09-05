a2f3823954feb800566f98ba0378afa201c36c9c
Felipe Dias <felipedias.101@outlook.com>
2024-09-05 15:06:32 -0300

    :wastebasket: remove class que não está mais sendo utilizadas
    Classes removidas: ClientController.cs; EmployeeController.cs; UserController.cs

--- SIV/Controllers/UserController.cs
+++ SIV/Controllers/UserController.cs
@@ -9,27 +9,7 @@ using SIV.Repositories;
 namespace SIV.Controllers;
 
 public class UserController
-{
-    public static DataTable GetAllUsers()
-    {
-        return UserRepository.GetAllUsers();
-    }
-
-    public static void SaveUser(User user)
-    {
-        UserRepository.SaveUser(user.Name, user.Password, user.Job, user.Access, user.Active, user.Permissions);
-    }
-
-    public static void UpdateUser(User user)
-    {
-        UserRepository.UpdateUser(user.Id, user.Name, user.Password, user.Job, user.Access, user.Active, user.Permissions);
-    }
-
-    public static void DeleteUser(string id)
-    {
-        UserRepository.DeleteUser(id);
-    }
-
+{ 
     public static void Logoff()
     {
         SessionManager.ClearSession();
@@ -38,6 +18,7 @@ public class UserController
     public static void Login(string username, string password)
     {
         var user = UserRepository.UserPermission(username, password);
+        
         if (user != null)
         {
             SessionManager.SetCurrentUser(user);
